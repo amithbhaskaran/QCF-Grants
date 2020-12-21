3d8b74bb57a28cc946b34f8c15ea320cd47ac85e
bamithprdxn <amithkumarb@prdxn.com>
2020-12-21 18:32:54 +0530

    js code for scroll added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,13 @@
-/* Author: 
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function () {
+    $(".navigation-links li a").click(function () {
+        $getElement = $(this).attr("href");
+        // checking if the section exists using length function
+        if ($($getElement).length) {
+            // storing top position
+            $getOffset = $($getElement).offset().top;
+            $("html, body").animate({
+                scrollTop: $getOffset
+            }, 1000)
+        }
+    });
+});
